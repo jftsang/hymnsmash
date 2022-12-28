822bf3b3629cbab376b2cf928d3e01f9cd3e498e
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2022-12-28 22:22:52 +0000

    Move voting section to a separate template file

---
+++ static/leaderboard.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    [].slice.call(document.querySelectorAll('span.leaderboard-competitor'))
+      .map(function (span) {
+          const cid = span.dataset['cid'];
+          span.addEventListener('mouseenter', async () => {
+            const r = await fetch('/competitor/' + cid);
+            const j = await r.json();
+            console.log(j);
+
+            span.setAttribute('data-bs-toggle', 'tooltip');
+            span.setAttribute('data-bs-html', 'true');
+            span.setAttribute('title', j.weight);
+          })
+        }
+      );
+
+  }
+);

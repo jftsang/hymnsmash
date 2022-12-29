ffca053dd6ab71eeac3162b2521fa00ed78968fe
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2022-12-29 19:23:04 +0000

    Move filters to their own module

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 import dotenv
 from flask import Flask
 
+import filters
 import views
 from models import db, Competitor, metadata, weight
 
@@ -18,43 +19,7 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-
-@app.template_filter()
-def format_elo(elo):
-    return str(elo)
-
-
-@app.template_filter()
-def format_delo(delo: int | None) -> str:
-    if delo is None:
-        return ''
-    return f'{delo:+d}'
-
-
-@app.template_filter()
-def hymn_number(competitor):
-    return metadata(competitor).get('number', '')
-
-
-@app.template_filter()
-def hymnary_url(competitor):
-    return f"https://hymnary.org/hymn/NEH/{hymn_number(competitor)}"
-
-
-@app.template_filter()
-def format_weight(competitor):
-    return f'{weight(competitor):.2f}'
-
-
-@app.template_filter()
-def describe_winloss(competitor):
-    wins, losses = competitor.wins, competitor.losses
-    if wins + losses:
-        perc = 100 * wins / (wins + losses)
-    else:
-        perc = 0
-    return f'{wins} - {losses}<br/>({perc:.1f}%)'
-
+filters.Filter.app_register(app)
 
 app.route('/')(views.index_view)
 app.route('/competitor')(views.competitor_list_view)
---
+++ filters.py
@@ -0,0 +1,52 @@
+from models import weight, metadata
+
+
+class Filter:
+    filters = []
+
+    @classmethod
+    def register(cls, f):
+        cls.filters.append(f)
+        return f
+
+    @classmethod
+    def app_register(cls, app):
+        for f in cls.filters:
+            app.template_filter()(f)
+
+
+@Filter.register
+def format_elo(elo):
+    return str(elo)
+
+
+@Filter.register
+def format_delo(delo: int | None) -> str:
+    if delo is None:
+        return ''
+    return f'{delo:+d}'
+
+
+@Filter.register
+def hymn_number(competitor):
+    return metadata(competitor).get('number', '')
+
+
+@Filter.register
+def hymnary_url(competitor):
+    return f"https://hymnary.org/hymn/NEH/{hymn_number(competitor)}"
+
+
+@Filter.register
+def format_weight(competitor):
+    return f'{weight(competitor):.2f}'
+
+
+@Filter.register
+def describe_winloss(competitor):
+    wins, losses = competitor.wins, competitor.losses
+    if wins + losses:
+        perc = 100 * wins / (wins + losses)
+    else:
+        perc = 0
+    return f'{wins} - {losses}<br/>({perc:.1f}%)'

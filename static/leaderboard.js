c212a615d80a8aeae21e027656009eb60f9bc2bd
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2023-05-08 16:33:26 +0100

    JS sorting of leaderboard

--- static/leaderboard.js
+++ static/leaderboard.js
@@ -15,3 +15,59 @@ document.addEventListener('DOMContentLoaded', () => {
       );
   }
 );
+
+
+/* https://javascript.info/task/sortable-table */
+
+document.addEventListener("DOMContentLoaded",
+  (e) => {
+    const leaderboard = document.getElementById("leaderboard");
+
+    function sortLeaderboard(evt) {
+      if (evt.target.tagName !== "TH")
+        return;
+
+      const th = evt.target;
+
+      function sortGrid(colNum, type) {
+        const currentSort = Number(leaderboard.dataset.currentSort);
+        let reverse = Boolean(leaderboard.dataset.reverse);
+        if (colNum === currentSort) {
+          reverse = !reverse;
+          leaderboard.dataset.reverse = reverse ? "yes" : "";
+        }
+        leaderboard.dataset.currentSort = colNum;
+
+        const tbody = leaderboard.querySelector("tbody");
+        const rowsArray = Array.from(tbody.rows);
+
+        function compare(rowA, rowB) {
+          const a = rowA.cells[colNum].innerHTML
+          const b = rowB.cells[colNum].innerHTML
+
+          let r;
+          if (type === "number")
+            r = b - a;  // sort descending
+          else if (type === "percentage")
+            r = Number(b.substring(0, b.length - 1))
+              - Number(a.substring(0, a.length - 1));
+          else if (type === "string")
+            r = (a > b) ? 1 : (a === b) ? 0 : -1;  // sort ascending
+          else
+            throw DOMException;
+
+          if (reverse)
+            r *= -1;
+          return r;
+        }
+
+        rowsArray.sort(compare);
+        tbody.append(...rowsArray);
+      }
+
+      sortGrid(th.cellIndex, th.dataset.type);
+    }
+
+    leaderboard.addEventListener("click", sortLeaderboard);
+  }
+);
